6e5c050a376506c12fb95061de2295ce1bcfca10
Liliya Blecher <liliyab@emg-soft.com>
2024-01-16 18:54:54 +0200

    new lesson2 created

---
+++ Homeworks/Homework1/homework1.py
@@ -0,0 +1,32 @@
+### Homework1: variables, list, list to set ###
+
+john_salary = 50000.456
+marta_salary = 50001.654
+
+print(john_salary, marta_salary)
+
+john_age = 25
+marta_age = 23
+
+print(john_age, marta_age)
+
+
+john_name = 'John'
+marta_name = 'Marta'
+
+print(john_name, marta_name)
+
+john_gender = False
+marta_gender = True
+
+print(john_gender, marta_gender)
+
+john_friends = ['James', 'Peter', 'Scott', 'Lucy']
+marta_friends = ['Julia', 'Maria', 'Jessy', 'Barbara']
+print(john_friends, marta_friends)
+
+names_list = ['Balin','Durin', 'Thorin', 'Dwalin', 'Gloin', 'Kili', 'Thorin', 'Balin']
+print(names_list)
+
+names_set = set(names_list)
+print(names_set)
